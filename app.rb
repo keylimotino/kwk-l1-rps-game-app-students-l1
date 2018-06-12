4f70092ee08c6087514b1bc75fa17362cec55047
keylimotino <stephanie48758@gmail.com>
2018-06-12 20:58:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require_relative 'config/environment'
 class GameApp < Sinatra::Base
   get '/rps/:play' do
     # Your app code goes here
+    def initialize
+      
+    end
     # Don't worry about this UNTIL you've passed all the tests!
     erb :rps_game
   end
